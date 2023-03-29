932ce74552bcae4a909baf5bf901675a27722c92
SUBASRI S <108917037+jananisri16@users.noreply.github.com>
2023-03-29 14:17:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Fruitsurvey from './Fruitsurvey';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <center>
+          <Fruitsurvey/>
+        </center>
+      </div>
+    )
+  }
+}
+export default App
